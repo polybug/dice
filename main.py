9f9d3d53aa8c0976d93997078b52cae90254655a
Deri Graves <104997276+polybug@users.noreply.github.com>
2022-05-05 15:02:55 +0000

    added score feature

--- main.py
+++ main.py
@@ -9,15 +9,24 @@ def diceRollP2():
 
 player1 = diceRollP1()
 player2 = diceRollP2()
+P1Score = 0
+P2Score = 0
 
 print(f'player 1 rolled a {player1}')
 print(f'player 2 rolled a {player2}')
+print(' ')
 
 if player1 > player2:
-  print('player 1 wins')
-
+  print(f'player 1 wins ({player1} over {player2})')
+  P1Score = (P1Score + 1)
+  print('')
+  print(f'player 1 score: {P1Score}                         player 2 score: {P2Score}')
 if player2 > player1:
-  print('player 2 wins')
-
+  print(f'player 2 wins ({player2} over {player1})')
+  P2Score = (P2Score + 1)
+  print('')
+  print(f'player 1 score: {P1Score}                         player 2 score: {P2Score}')
 if player1 == player2:
-  print('draw')
+  print(f'draw! ({player1} and {player2})')
+  print('')
+  print(f'player 1 score: {P1Score}                         player 2 score: {P2Score}')
