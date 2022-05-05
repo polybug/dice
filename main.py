7be425cd60a72253db4dd837034119005c312d75
Deri Graves <104997276+polybug@users.noreply.github.com>
2022-05-05 14:51:06 +0000

    reworked script to use functions and added a draw and win feature

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import random
-diceNumber = random.randint(1,6)
 
-print(diceNumber)
+def diceRollP1():
+ return random.randint(1,6)
 
-if diceNumber == 6:
-  print('you win you are now cool')
+def diceRollP2():
+ return random.randint(1,6)
+
+
+player1 = diceRollP1()
+player2 = diceRollP2()
+
+print(f'player 1 rolled a {player1}')
+print(f'player 2 rolled a {player2}')
+
+if player1 > player2:
+  print('player 1 wins')
+
+if player2 > player1:
+  print('player 2 wins')
+
+if player1 == player2:
+  print('draw')
